e46237759be5f5a3146743eaf29172211bc3e668
Josh Silverman <joshs.silverman@gmail.com>
2014-05-02 21:57:49 -0400

    table view with custom class

---
+++ Wisr/Askers/WSRAskersViewController.h
@@ -0,0 +1,13 @@
+//
+//  WSRAskersViewController.h
+//  Wisr
+//
+//  Created by Josh Silverman on 5/2/14.
+//  Copyright (c) 2014 Wisr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSRAskersViewController : UITableViewController
+
+@end
---
+++ Wisr/Askers/WSRAskersViewController.m
@@ -0,0 +1,13 @@
+//
+//  WSRAskersViewController.m
+//  Wisr
+//
+//  Created by Josh Silverman on 5/2/14.
+//  Copyright (c) 2014 Wisr. All rights reserved.
+//
+
+#import "WSRAskersViewController.h"
+
+@implementation WSRAskersViewController
+
+@end
