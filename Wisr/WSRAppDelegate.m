e1ff5e4e802b0515b876803de4864dbf8a487090
Josh Silverman <joshs.silverman@gmail.com>
2014-07-19 12:40:11 -0400

    app registers for push notifications

--- Wisr/WSRAppDelegate.m
+++ Wisr/WSRAppDelegate.m
@@ -17,7 +17,20 @@
 {
     [self initAppearance];
     
+	[[UIApplication sharedApplication] registerForRemoteNotificationTypes:
+     (UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    
     return YES;
+}
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken
+{
+	NSLog(@"My token is: %@", deviceToken);
+}
+
+- (void)application:(UIApplication*)application didFailToRegisterForRemoteNotificationsWithError:(NSError*)error
+{
+	NSLog(@"Failed to get token, error: %@", error);
 }
 
 - (void)initAppearance
